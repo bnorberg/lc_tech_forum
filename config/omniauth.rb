3206cfe823832eb8239bb9e8685683fd158781fd
Brian Norberg <bnorberg02@gmail.com>
2014-09-19 11:40:25 -0400

    Delete omniauth.rb

--- config/omniauth.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  #production key
-  provider :google_oauth2, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'], {access_type: 'online', approval_prompt: ''}
-  #development key
-  #provider :google_oauth2, ENV['LCTECH_G_KEY'], ENV['LCTECH_G_SECRET'], {access_type: 'online', approval_prompt: ''}
-end
